a37b2ff208c5db72a92c7a022c68efe92a0fe023
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-08-13 13:36:00 +0300

    add weapon command

--- main.py
+++ main.py
@@ -11,4 +11,23 @@ def hello(message):
     bot.send_message(message.chat.id, "Привет!")
 
 
+@bot.message_handler(commands=['weapon', 'оружие'])
+def info_weapon(message):
+    if len(message.text.split()) == 1:
+        bot.send_message(message.chat.id, 'Укажите оружие')
+    elif len(message.text.split()) == 2:
+        weapon = message.text.split()[1]
+        bot.send_message(message.chat.id, weapon)
+    elif len(message.text.split()) >= 3:
+        bot.send_message(message.chat.id, "Выберите только 1 оружие")
+
+
+@bot.message_handler(commands=['викторина', 'quiz'])
+def quiz(message):
+    bot.send_message(message.chat.id, 'Quiz now is in development')
+    # bot.send_poll(
+    # message.chat.id, 'Какой калибр у АК-47?', ['5.56x45', '7.62x39', '7.62x54'], correct_option_id=1, open_period=30
+    # )
+
+
 bot.polling()
